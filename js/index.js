7dbe0356ced402e237793e8b9d25e9fb16839af1
sevenblackthorns <161628029+sevenblackthorns@users.noreply.github.com>
2024-04-09 10:57:18 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,5 @@ import {ArrayShape} from "arraymath.js";
 console.log(ArrayShape([0, 1, 2]));
 console.log(ArrayShape([[1, 3], [3, 3]]));
 console.log(ArrayShape([[[0], [0], [0]], [[0], [0], [0]]]));
+console.log(ArrayShape([]));
+console.log(ArrayShape([1, [1]]));
